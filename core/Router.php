87ce10484f4484cf0db318f9101343c9c005a844
lahcen <lahcen.ben3@gmail.com>
2023-12-20 15:01:02 +0100

    Added 'Router.php' to handle URI parsing and routing. Updated 'index.php' to initialize and use the 'Router' class for handling requests.

--- core/Router.php
+++ core/Router.php
@@ -1,6 +1,58 @@
 <?php
 
-class Router{
+class Router
+{
 
-    
+    private $controller = 'App\Controller\EquipController';
+    private $method = 'index';
+    private $params = [];
+
+    public function __construct()
+    {
+        $this->parseUri();
+    }
+
+    public function parseUri()
+    {
+        $uri = $_GET['uri'] ?? '';
+        $uri = explode('/', trim(strtolower($uri), '/'));
+
+        if (!empty($uri[0])) {
+
+            $controller = $uri[0] . 'Controller';
+            unset($uri[0]);
+            $controller = 'App\Controller\\' . $controller;
+
+            if (class_exists($controller)) {
+                $this->controller = $controller;
+            } else {
+                echo "error not fined";
+                exit;
+            }
+        }
+
+        $class = $this->controller;
+        $class = new $class;
+
+
+        if (isset($uri[1])) {
+
+            $method = $uri[1];
+            unset($uri[1]);
+
+            if (method_exists($class, $method)) {
+                $this->method = $method;
+            } else {
+                echo "error not fined";
+                exit;
+            }
+        }
+
+
+        if (isset($uri[2])) {
+            $this->params = array_values($uri);
+        }
+
+        call_user_func_array([$class, $this->method], $this->params);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
- include '../core/config.php';
- 
+
+
+include '../core/config.php';
+include '../core/function.php';
+require '../core/Router.php';
+//  include '../app/View/errors/404.php';
+
+new Router;
+
