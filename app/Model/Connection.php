6b22f21d3edec6611531cf34d4b0d2bd23874205
lahcen <lahcen.ben3@gmail.com>
2023-12-23 16:53:46 +0100

    Create a team model that encompasses all controller crud functions.

--- app/Model/Connection.php
+++ app/Model/Connection.php
@@ -3,18 +3,41 @@
 namespace App\Model;
 
 use PDO;
+use PDOException;
 
-class Connection{
-    public $con;
+class Connection
+{
+    public $pdo;
 
     public function __construct()
     {
         $this->connect();
     }
-    public function connect(){
-        if (!$this->con) {
-            $this->con = new PDO('mysql:host='.DB_HOST.';dbname='.DB_USER,DB_PASS,DB_NAME);
+    public function connect()
+    {
+
+        if (!$this->pdo) {
+            try {
+                $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+            } catch (PDOException $e) {
+                $this->logError($e);
+                return null;
+            }
         }
     }
-}
+    
+    protected function logError(PDOException $e)
+    {
+        $logFilePath = dirname(__DIR__ . '../') . '\logs\error.log';
+        $errorMessage = "[" . date('Y-m-d H:i:s') . "] " . $e->getMessage() . "\n";
+        file_put_contents($logFilePath, $errorMessage, FILE_APPEND);
+    }
 
+    protected function logQuery($query)
+    {
+        $logFilePath = dirname(__DIR__ . '../') . '\logs\query.log';
+
+        $logMessage = "[" . date('Y-m-d H:i:s') . "] " .  $query . "\n";
+        file_put_contents($logFilePath, $logMessage, FILE_APPEND);
+    }
+}
---
+++ app/Model/Team.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Model;
+
+class Team extends Crud{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function selectAllTeam(){
+        return $this->selectRecords('teams');
+    }
+    public function selectTeam($id){
+        return $this->selectRecords('teams','*','id = '.$id);
+    }
+    public function addTeam(array $data){
+        return $this->insertRecord('teams',$data);
+    }
+    public function DeleteTeam(int $id){
+        return $this->deleteRecord('teams',$id);
+    }
+    public function UpdateTeam(array $data,int $id){
+        return $this->updateRecord('teams',$data,$id);
+    }
+}
